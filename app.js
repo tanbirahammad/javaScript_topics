0e22890d65cabff185c4568092d3b7bc8080e860
tanbirahammad <jht017933@gmail.com>
2022-07-23 03:52:43 -0700

    learn destructuring

---
+++ app.js
@@ -0,0 +1,25 @@
+// destructuring
+
+// const user={
+//     id:335,
+//     name: 'Tanvir',
+//     age:'25',
+//     education:{
+//         degree:'Bachelor',
+//         sub:'Computer Science',
+//         projects:{
+//             major:'software Development',
+//             // minor:'artificial intelligence',
+//         }
+//      }
+// };
+
+// const{education:{projects:{minor}} } =user;
+// console.log(minor);
+
+// Array destructuring
+
+
+
+
+
