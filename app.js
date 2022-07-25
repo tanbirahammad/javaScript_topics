b8de9bbbf51fd5e15ba128a8b904ea3eb732c7dc
tanbirahammad <jht017933@gmail.com>
2022-07-25 14:50:38 -0700

    class basic clear

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
-// Rest operator
+// Class
 
-// Rest parameter must be last formal parameter 
+class Car {
+    constructor(name, year) {
+      this.name = name;
+      this.year = year;
+    }
+    run(){
+        console.log(`${this.name} top Speed is 150kmph`);
+    }
+  }
 
-function restApi(a, ...param){
-    console.log(param)
-    console.log(a)
-}
+  const car1=new Car("MT-15",2022); // Create actual object.
+  const car2=new Car("R15-V3",2020); 
+  const car3=new Car("R15-V4",2022);
 
-
-restApi(1,2,3,4,5)
+ car1.run();
+ car2.run();
+ car3.run();
+  
